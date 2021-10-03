42e3565bdb4e18a8088ae114bc2df85b93b44758
ivanandika21 <ivanandikasurya21@gmail.com>
2021-10-03 11:30:13 +0700

    Update Latihan2-Tabel.php

--- WPU-Pertemuan3/Latihan2-Tabel.php
+++ WPU-Pertemuan3/Latihan2-Tabel.php
@@ -11,7 +11,6 @@
         <?php
             for($i = 1; $i <= 3; $i++){
                 echo "<tr>";
-                
                 for($j = 1; $j <= 5; $j++){
                     echo "<td>";
                     echo $i.", ".$j;
