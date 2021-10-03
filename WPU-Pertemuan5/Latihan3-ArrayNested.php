db0f9175fd68b8c382bb13867302b23bd6e871d8
ivanandika21 <ivanandikasurya21@gmail.com>
2021-10-03 23:33:45 +0700

    Update Latihan3-ArrayNested.php

--- WPU-Pertemuan5/Latihan3-ArrayNested.php
+++ WPU-Pertemuan5/Latihan3-ArrayNested.php
@@ -13,12 +13,12 @@
 <body>
     <h1>Daftar Mahasiswa</h1>
     <?php foreach ($mahasiswa as $mhs) : ?>
-    <ul>
-        <li>Nama : <?= $mhs[0] ?></li>
-        <li>NIM : <?= $mhs[1] ?></li>
-        <li>Progdi : <?= $mhs[2] ?></li>
-        <li>Email : <?= $mhs[3] ?></li>
-    </ul>
+        <ul>
+            <li>Nama : <?= $mhs[0] ?></li>
+            <li>NIM : <?= $mhs[1] ?></li>
+            <li>Progdi : <?= $mhs[2] ?></li>
+            <li>Email : <?= $mhs[3] ?></li>
+        </ul>
     <?php endforeach; ?>
 </body>
 </html>
