afd8d406ed7fdf3fc9ad9397b141b933f38bb45c
ivanandika21 <ivanandikasurya21@gmail.com>
2021-10-03 11:23:40 +0700

    Upload file pertemuan 1 dan 3

---
+++ WPU-Pertemuan3/Latihan1-Perulangan.php
@@ -0,0 +1,25 @@
+<?php
+    // Perulangan
+
+    // for
+    for($i = 0; $i < 5; $i++){
+        echo "Hello World 1!<br>";
+    }
+
+    // while
+    $i = 0;
+    while($i < 5){
+        echo "Hello World 2!<br>";
+        $i++;
+    }
+
+    // do .. while
+    $i = 0;
+    do{
+        echo "Hello World 3!<br>";
+        $i++;
+    } while($i < 5);
+    
+    // foreach (perulangan khusus array)
+
+?>
---
+++ WPU-Pertemuan3/Latihan3-Pengkondisian.php
@@ -0,0 +1,13 @@
+<?php
+    // Pengkondisian
+
+    // if else
+    $x = 10;
+    if ($x < 20){
+        echo "Benarr";
+    } else if ($x == 10){
+        echo "Angka 10 pas"
+    } else {
+        echo "Salahh"
+    }
+?>
